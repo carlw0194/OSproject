5118dd44424effdd1d8d6f83a9c336fb93217334
carlw0194 <96522018+carlw0194@users.noreply.github.com>
2022-01-01 11:30:08 +0200

    Update scheduling.c

--- scheduling.c
+++ scheduling.c
@@ -1,62 +1,21 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-typedef struct process{
-   char name[5];
-   int burstTime;
-   int arrivalTime;
-   int priority;
-   int waitingTime,turnAround,flag;
-
-}processes;
-
-int accept(processes P[]){
-int i,n;
-printf("\n Enter total no. of processes : ");
-   scanf("%d",&n);
-   for(i=0;i<n;i++){
-       printf("\n PROCESS [%d]",i+1);
-       printf(" Enter process name : ");
-       scanf("%s",&P[i].name);
-       printf(" Enter burst time : ");
-       scanf("%d",&P[i].burstTime);
-       printf(" Enter arrival time : ");
-       scanf("%d",&P[i].arrivalTime);
-       printf(" Enter priority : ");
-       scanf("%d",&P[i].priority);
-   }
-   printf("\n PROG.\tB.T.\tA.T.\tPRIORITY");
-   for(i=0;i<n;i++)
-       printf("\n %s\t%d\t%d\t%d",P[i].name,P[i].burstTime,P[i].arrivalTime,P[i].priority);
-   return n;
-}
-
-
-void sortFunction(processes temp[],int n)
-{
-   processes x;
-   int i,j;
-   for(i=1;i<n;i++)
-       for(j=0;j<n-i;j++){
-           if(temp[j].arrivalTime > temp[j+1].arrivalTime){
-               x = temp[j];
-               temp[j] = temp[j+1];
-               temp[j+1] = x;
-           }
-       }
-}
-
-
 int main(){
 
    processes P[10];
    int choice,n;
    do{
-       printf("\n\n  CPU SCHEDULING ALGORITHMS SIMULATION \n");
+       printf("\n\n CPU SCHEDULING ALGORITHMS SIMULATION \n");
        printf("\n Options:");
        printf("\n 0. Enter process data.");
-       printf("\n 1. FCFS\n");
-
+       printf("\n 1. FCFS");
+       printf("\n 2. SJF (Preemptive)");
+       printf("\n 3. SJF (Non Preemptive)");
+       printf("\n 4. Priority Scheduling (Preemptive)");
+       printf("\n 5. Priority Scheduling (Non Preemptive)");
+       printf("\n 6. Round Robin");
+       printf("\n 7. Exit\n Select : ");
        scanf("%d",&choice);
        switch(choice){
            case 0:
@@ -65,10 +24,24 @@ int main(){
            case 1:
                FCFS(P,n);
                break;
-
+           case 2:
+               sjfPreemptive(P,n);
+               break;
+           case 3:
+               SJF_NP(P,n);
+               break;
+           case 4:
+               PRT_P(P,n);
+               break;
+           case 5:
+               PRT_NP(P,n);
+               break;
+           case 6:
+               RR(P,n);
+               break;
+           case 7:exit(0);
        }
-   }while(choice <! 0 || choice >!1);
-   getchar();
+   }while(choice != 7);
 
    return 0;
 }
